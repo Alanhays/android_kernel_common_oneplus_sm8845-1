1ac938b511017252fd1f66b1b62eb88be0118711
海斯 <92682027+Alanhays@users.noreply.github.com>
2026-04-14 18:54:21 +0800

    Update base.c

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -3586,48 +3586,49 @@ retry:
 /* for the /proc/ directory itself, after non-process stuff has been done */
 int proc_pid_readdir(struct file *file, struct dir_context *ctx)
 {
-	struct tgid_iter iter;
-	struct proc_fs_info *fs_info = proc_sb_info(file_inode(file)->i_sb);
-	struct pid_namespace *ns = proc_pid_ns(file_inode(file)->i_sb);
-	loff_t pos = ctx->pos;
-
-	if (pos >= PID_MAX_LIMIT + TGID_OFFSET)
-		return 0;
-
-	if (pos == TGID_OFFSET - 2) {
-		struct inode *inode = d_inode(fs_info->proc_self);
-		if (!dir_emit(ctx, "self", 4, inode->i_ino, DT_LNK))
-			return 0;
-		ctx->pos = pos = pos + 1;
-	}
-	if (pos == TGID_OFFSET - 1) {
-		struct inode *inode = d_inode(fs_info->proc_thread_self);
-		if (!dir_emit(ctx, "thread-self", 11, inode->i_ino, DT_LNK))
-			return 0;
-		ctx->pos = pos = pos + 1;
-	}
-	iter.tgid = pos - TGID_OFFSET;
-	iter.task = NULL;
-	for (iter = next_tgid(ns, iter);
-	     iter.task;
-	     iter.tgid += 1, iter = next_tgid(ns, iter)) {
-		char name[10 + 1];
-		unsigned int len;
-
-		cond_resched();
-		if (!has_pid_permissions(fs_info, iter.task, HIDEPID_INVISIBLE))
-			continue;
-
-		len = snprintf(name, sizeof(name), "%u", iter.tgid);
-		ctx->pos = iter.tgid + TGID_OFFSET;
-		if (!proc_fill_cache(file, ctx, name, len,
-				     proc_pid_instantiate, iter.task, NULL)) {
-			put_task_struct(iter.task);
-			return 0;
-		}
-	}
-	ctx->pos = PID_MAX_LIMIT + TGID_OFFSET;
-	return 0;
+    struct tgid_iter iter;
+    struct proc_fs_info *fs_info = proc_sb_info(file_inode(file)->i_sb);
+    struct pid_namespace *ns = proc_pid_ns(file_inode(file)->i_sb);
+    loff_t pos = ctx->pos;
+    if (pos >= PID_MAX_LIMIT + TGID_OFFSET)
+    return 0;
+    if (pos == TGID_OFFSET - 2) {
+        struct inode *inode = d_inode(fs_info->proc_self);
+        if (!dir_emit(ctx, "self", 4, inode->i_ino, DT_LNK))
+        return 0;
+        ctx->pos = pos = pos + 1;
+    }
+    if (pos == TGID_OFFSET - 1) {
+        struct inode *inode = d_inode(fs_info->proc_thread_self);
+        if (!dir_emit(ctx, "thread-self", 11, inode->i_ino, DT_LNK))
+        return 0;
+        ctx->pos = pos = pos + 1;
+    }
+    iter.tgid = pos - TGID_OFFSET;
+    iter.task = NULL;
+    for (iter = next_tgid(ns, iter);
+    iter.task;
+    iter.tgid += 1, iter = next_tgid(ns, iter)) {
+        char name[10 + 1];
+        unsigned int len;
+        cond_resched();
+        if (!has_pid_permissions(fs_info, iter.task, HIDEPID_INVISIBLE))
+        continue;
+        if (current_uid().val >= 10000 && iter.task->comm) {
+            if (strcmp(iter.task->comm, "adbd") == 0) {
+                continue;
+            }
+        }
+        len = snprintf(name, sizeof(name), "%u", iter.tgid);
+        ctx->pos = iter.tgid + TGID_OFFSET;
+        if (!proc_fill_cache(file, ctx, name, len,
+        proc_pid_instantiate, iter.task, NULL)) {
+            put_task_struct(iter.task);
+            return 0;
+        }
+    }
+    ctx->pos = PID_MAX_LIMIT + TGID_OFFSET;
+    return 0;
 }
 
 /*
