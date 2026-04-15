98c93d50a31de7cb0e389a2a1ee1c134a7d93404
海斯 <92682027+Alanhays@users.noreply.github.com>
2026-04-15 11:12:26 +0800

    Update cmdline.c

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -3,30 +3,42 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <linux/string.h>
+#include <linux/slab.h>
 #include "internal.h"
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-    if (current_uid().val < 10000) {
-        seq_puts(m, saved_command_line);
-    } else {
-        char *p_buf = kstrdup(saved_command_line, GFP_KERNEL);
-        char *target;
+    char *orig_cmdline = saved_command_line;
+    char *new_cmdline;
+    char *pos;
+    char *end;
 
-        if (p_buf) {
-            while ((target = strstr(p_buf, "androidboot.debuggable=1")) != NULL) {
-                memset(target, ' ', 24);
-            }
-            while ((target = strstr(p_buf, "androidboot.selinux=permissive")) != NULL) {
-                memset(target, ' ', 30);
-            }
-            seq_puts(m, p_buf);
-            kfree(p_buf);
-        } else {
-            seq_puts(m, saved_command_line);
+    new_cmdline = kstrdup(orig_cmdline, GFP_KERNEL);
+    if (!new_cmdline)
+        return -ENOMEM;
+
+    pos = strstr(new_cmdline, "androidboot.serialno=");
+    if (pos) {
+        char *val_start = pos + strlen("androidboot.serialno=");
+        
+        end = strchr(val_start, ' ');
+        
+        if (val_start) {
+            char *prefix = kstrndup(new_cmdline, val_start - new_cmdline, GFP_KERNEL);
+            char *suffix = end ? kstrdup(end, GFP_KERNEL) : kstrdup("", GFP_KERNEL);
+
+            seq_printf(m, "%s3G456U11FH700000%s\n", prefix, suffix);
+
+            kfree(prefix);
+            kfree(suffix);
+            kfree(new_cmdline);
+            return 0;
         }
     }
-    seq_putc(m, '\n');
+
+    seq_printf(m, "%s\n", saved_command_line);
+    kfree(new_cmdline);
     return 0;
 }
 
